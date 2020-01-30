f04a2e21a00e7912c81b144c901bceb1300f5226
alfred williams <alfredfamu97@gmail.com>
2020-01-30 01:15:57 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Iteration_Statment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                 // Ask the user for input
+
+            Console.WriteLine("Enter an Integer value between 1 and 100 to execute a For Loop: ");
+
+            /*
+            Use the try catch block to validate user input.
+            If the user provides bad input, the catch block
+            will handle the error and a message will be displayed.
+            */
+
+            try
+            {
+                // This variable will gather data from user input 
+
+                string input = Console.ReadLine();
+
+                // This variable will be used to perform the various iterative statements and is parsed as an integer
+
+                int value_of_input = int.Parse(input);
+
+                /* 
+                This conditional IF/ELSE IF/ELSE statement is used to test the values of user input.
+                Depending on the value of the user input between 1 and 100, a For Loop iterative statement will execute. 
+                */
+
+                // If the value of user input is between 1 and 100, execute a For Loop
+
+                if ((value_of_input > 0) && (value_of_input <= 100))
+
+                
+
+                    // Here is the For Loop
+
+                for (int i = 0; i < value_of_input; i++)
+
+                    {
+                Console.WriteLine("This is the current integer value in the loop:" + i.ToString());
+                    }
+
+                    // Pause the program and await the user to press a key to end the program
+
+                    Console.ReadKey(true);
+                
+            }
+            catch
+
+            {
+
+                Console.WriteLine("Please enter an integer value and try running the program again ...");
+
+                Console.WriteLine("Press any key to exit the program ...");
+
+                Console.ReadKey(true);
+
+            }
+            }
+    }
+}
